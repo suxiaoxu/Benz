21ddb4b6030d830a96eac220b3328ba10b9aa418
xiaoxu.sxx <xiaoxu.sxx@alibaba-inc.com>
2016-11-27 11:23:29 +0800

    1

--- benz/benz-client/src/test/java/com/util/date/Test.java
+++ benz/benz-client/src/test/java/com/util/date/Test.java
@@ -11,7 +11,7 @@ public class Test {
 			Sn sn = new Sn();
 			sn.print(2);
 			System.out.println("testa22");
-			System.out.println("testa12");
+			System.out.println("testa1sdf2");
 			System.out.println("ceshi weenti");
 			System.out.println("ceshi wenti");
 		}
@@ -20,7 +20,24 @@ public class Test {
 
 }
 
+/** 
+* @ClassName: Sn 
+* @Description: TODO(这里用一句话描述这个类的作用) 
+* @author xiaoxu.sxx 
+* @date 2016年8月1日 下午9:19:02 
+* 
+*/
 class Sn{
+	/**
+	 * @param a
+	 */
+	/** 
+	* @Title: print 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @param a    设定文件 
+	* @return void    返回类型 
+	* @throws 
+	*/
 	public void print(int a){
 		Tn tn = new Tn();
 		try {
@@ -29,6 +46,57 @@ class Sn{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	/** 
+	* @Title: Sn 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param     设定文件 
+	* @return TODO    返回类型 
+	* @throws 
+	*/
+	public static void mian()
+	{
+		
+	}
+	
+	/** 
+	* @Title: getName 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param a leixing1
+	* @param b leixing2
+	* @return int    返回类型 
+	* @throws 
+	*/
+	public int getName(int a,String b){
+		return 1;
+	}
+	
+	/** 
+	* @Title: sub 
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @param a
+	* @param @param b
+	* @param @return    设定文件 
+	* @return int    返回类型 
+	* @throws 
+	*/
+	private int sub(int a,int b){
+		int c = a-b;
+		return c;
+	}
+	
+	/** 
+	* @Title: test =this  
+	* @Description: TODO(这里用一句话描述这个方法的作用) 
+	* @param @param a
+	* @param @param b
+	* @param @return    设定文件 
+	* @return int    返回类型 
+	* @throws 
+	*/
+	public int snn(int a,String b){
+		return a;
 	}
 }
 
